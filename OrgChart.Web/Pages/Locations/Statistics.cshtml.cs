1657a3c7ff42d09e3d0ea250f7df2b68b4912791
ColinWaldner <73153023+ColinWaldner@users.noreply.github.com>
2023-05-24 15:50:08 -0500

    Statistics Page

---
+++ OrgChart.Web/Pages/Locations/Statistics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrgChart.Web.Pages.Locations
+{
+    public class StatisticsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
